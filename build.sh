bcd08905eb1114381b8eae62cd0b25aeeb0d7ea9
Philip Geurin <philip.geurin@gmail.com>
2025-10-14 20:13:13 -0700

    Fix build script to explicitly install gunicorn

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 # Install dependencies
-pip install -r requirements.prod.txt
+pip install -r requirements.txt
+pip install gunicorn==21.2.0
 
 # Collect static files
 python django_app/manage.py collectstatic --no-input
